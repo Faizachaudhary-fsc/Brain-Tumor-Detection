d3585ed31fff198b9708886f4e6a91588f3d1019
Faiza Shakeel Chaudhary <faizashakeelchaudhary333@gmail.com>
2024-11-09 08:37:36 +0000

    5th commit

--- streamlitapp.py
+++ streamlitapp.py
@@ -1,14 +1,10 @@
 import streamlit as st
-import pickle
 import tensorflow as tf
 from tensorflow.keras.preprocessing.image import img_to_array
 import numpy as np
 from PIL import Image
 
 # Load the saved models
-with open('svm_model1.pkl', 'rb') as f:
-    svm_model = pickle.load(f)
-
 cnn_model = tf.keras.models.load_model('cnn_model1.h5')
 resnet50_model = tf.keras.models.load_model('resnet50_model.h5')
 
@@ -35,19 +31,16 @@ if uploaded_file is not None:
     preprocessed_image = preprocess_image(image)
 
     # Select Model
-    model_choice = st.selectbox("Choose a model for prediction:", ["CNN", "ResNet50", "SVM"])
+    model_choice = st.selectbox("Choose a model for prediction:", ["CNN", "ResNet50"])
 
     # Make prediction based on model choice
     if model_choice == "CNN":
         prediction = cnn_model.predict(preprocessed_image)
-        prediction_class = np.argmax(prediction, axis=1)[0]
     elif model_choice == "ResNet50":
         prediction = resnet50_model.predict(preprocessed_image)
-        prediction_class = np.argmax(prediction, axis=1)[0]
-    elif model_choice == "SVM":
-        # Flatten the image for SVM (if required)
-        flattened_image = preprocessed_image.flatten().reshape(1, -1)
-        prediction_class = svm_model.predict(flattened_image)[0]
+    
+    # Get prediction class
+    prediction_class = np.argmax(prediction, axis=1)[0]
 
     # Show the result
     if prediction_class == 1:

6914df00151e1430a68ca5a1490db90e69a2907c
Faiza Shakeel Chaudhary <faizashakeelchaudhary333@gmail.com>
2024-11-13 12:58:56 +0000

    Added files

--- streamlitapp.py
+++ streamlitapp.py
@@ -4,19 +4,26 @@ import numpy as np
 import cv2
 
 # Load the CNN model
-cnn_model = load_model('brain_tumor_cnn_model.h5')
+cnn_model = load_model('cnn_model1.h5')
 
 st.title("Brain Tumor Detection")
 uploaded_file = st.file_uploader("Upload MRI Image", type="jpg")
 
 if uploaded_file:
+    # Read and decode the uploaded image file
     file_bytes = np.asarray(bytearray(uploaded_file.read()), dtype=np.uint8)
-    img = cv2.imdecode(file_bytes, 1)
-    img_resized = cv2.resize(cv2.cvtColor(img, cv2.COLOR_BGR2GRAY), (128, 128))
-    img_normalized = img_resized.reshape(1, 128, 128, 1) / 255.0
+    img = cv2.imdecode(file_bytes, cv2.IMREAD_COLOR)
+    
+    # Resize to 128x128 and normalize for the model input
+    img_resized = cv2.resize(img, (128, 128))
+    img_normalized = img_resized.reshape(1, 128, 128, 3) / 255.0
 
+    # Get prediction
     prediction = cnn_model.predict(img_normalized)
-    if prediction > 0.5:
+    class_index = np.argmax(prediction, axis=1)[0]  # Get the index of the highest probability
+
+    # Display results based on prediction
+    if class_index == 1:
         st.write("Tumor Detected")
     else:
         st.write("No Tumor Detected")
